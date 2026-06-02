7f8666487d3c9a9c1b431e80bf8ff265614c1c42
Kondeti Vijaya Durga <durgakondeti1820@gmail.com>
2026-06-02 15:24:38 +0530

    Array Solutions

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,15 @@
+public class LinearSearch {
+    static int linearSearch(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {10,20,30,40};
+        int target = 30;
+        System.out.println(linearSearch(nums,target));
+    }
+}
