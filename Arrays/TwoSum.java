9536cc720aed937f3d403f0e512e0e09666363d4
Kondeti Vijaya Durga <durgakondeti1820@gmail.com>
2026-06-05 19:02:43 +0530

    Array Solutions

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,42 @@
+/* //Brute Force Approach
+import java.util.*;
+public class TwoSum {
+    static int[] twosum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++) {
+            for(int j=i+1;j<nums.length;j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.print(Arrays.toString(twosum(nums,target)));
+    }
+} */
+
+//Optimal Approach
+import java.util.*;
+public class TwoSum {
+    static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            int complement = target - nums[i];
+            if(map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1,-1};
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.print(Arrays.toString(twoSum(nums,target)));
+    }
+}
+
